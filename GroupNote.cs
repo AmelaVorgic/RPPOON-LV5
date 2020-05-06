91ddd74475149f94832efbb0e87cc9928557e2de
Amela Vorgić <Korisnik@DESKTOP-CON3K37>
2020-05-06 16:22:19 +0200

    .

---
+++ GroupNote.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV5RPPOON
+{
+    class GroupNote : Note
+    {
+        List<string> authors;
+        public GroupNote(string message, string mainAuthor, ITheme theme) : base(message, theme)
+        {
+            this.authors = new List<string>();
+            this.authors.Add(mainAuthor);
+        }
+
+        public override void Show()
+        {
+            this.ChangeColor();
+            Console.WriteLine("GROUP REMINDER: ");
+            string framedMessage = this.GetFramedMessage();
+            Console.WriteLine(framedMessage);
+            Console.WriteLine("Authors:" + AuthorsToString(this.authors));
+            Console.ResetColor();
+        }
+        public void Add(string newAuthor)
+        {
+            if (this.authors == null)
+            {
+                this.authors = new List<string>();
+                this.authors.Add(newAuthor);
+            }
+            else
+                this.authors.Add(newAuthor);
+        }
+        public void Remove(string removeAuthor)
+        {
+            if (this.authors != null)
+            {
+                if (this.authors.Contains(removeAuthor))
+                {
+                    int i = authors.IndexOf(removeAuthor);
+                    authors.RemoveAt(i);
+                }
+            }
+        }
+        public string AuthorsToString(List<string> authors)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (string author in this.authors)
+            {
+                stringBuilder.Append(author + ", ");
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace LV5RPPOON
     {
         static void Main(string[] args)
         {
+            /*
+            //zadatak 5
             LightTheme lightTheme = new LightTheme();
             MyTheme yellowTheme = new MyTheme();
             ReminderNote firstnote = new ReminderNote("Is this working?", lightTheme);
@@ -17,6 +19,67 @@ namespace LV5RPPOON
             ReminderNote secondnote = new ReminderNote("This one is pretty!", yellowTheme);
             secondnote.Show();
             Console.ReadKey();
+            */
+
+            /*
+            //zadatak 6
+            LightTheme lightTheme = new LightTheme();
+            MyTheme yellowTheme = new MyTheme();
+
+            GroupNote Flathearthers = new GroupNote("Earth is flat!", "Macht", lightTheme);
+            Flathearthers.Add("Labbie");
+            Flathearthers.Add("Edith");
+            Flathearthers.Add("Trump");
+            Flathearthers.Show;
+
+            GroupNote Presidents = new GroupNote("I do solemnly swear that I will faithfully execute the Office of President.", "Obama", yellowTheme);
+            Presidents.Add("Putin");
+            Presidents.Add("Vucic");
+            Presidents.Add("Grabar Kitarovic");
+            Presidents.Add("Milanovic");
+            Presidents.Show;
+
+            Flathearthers.Remove("Edith");
+            Flathearthers.Remove("Donald");
+            Flathearthers.Show();
+
+            Presidents.Remove("Grabar Kitarovic");
+            Presidents.Add("Trump");
+            Presidents.Show();
+
+            Console.ReadKey();
+            */
+
+            //zadatak7
+            LightTheme lightTheme = new LightTheme();
+            MyTheme yellowTheme = new MyTheme();
+
+            GroupNote Doctors = new GroupNote("U casu kada stupam medu clanove lijecnicke profesije, svecano obecajem da cu svoj zivot staviti u sluzbu humanosti", "Hipokrat", lightTheme);
+            Doctors.Add("Markotic");
+            Doctors.Add("Beros");
+            Doctors.Add("Bahtijarevic");
+
+            GroupNote MinistersOfHealth = new GroupNote("Tako mi Bog pomogao", "Hebrang", yellowTheme);
+            MinistersOfHealth.Add("Ostojic");
+            MinistersOfHealth.Add("Milinovic");
+            MinistersOfHealth.Add("Kujundzic");
+
+            MinistersOfHealth.Remove("Kujundzic");
+            MinistersOfHealth.Add("Beros");
+            
+            Notebook firstnotebook = new Notebook();
+            notebook.AddNote(MinistersOfHealth);
+            notebook.AddNote(Doctors);
+            notebook.Display();
+
+            notebook = new Notebook(yellowTheme);
+            notebook.AddNote(MinistersOfHealth);
+            notebook.AddNote(Doctors);
+            notebook.Display(); ;
+
+            Console.ReadKey();
+
+
         }
     }
 }
