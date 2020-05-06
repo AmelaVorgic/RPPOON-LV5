60b0ed76120802e409705efe1cb64376b21ad2e8
Amela Vorgić <Korisnik@DESKTOP-CON3K37>
2020-05-06 13:50:24 +0200

    zadatak 5

---
+++ MyTheme.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV5RPPOON
+{
+    class MyTheme : ITheme
+    {
+        public void SetBackgroundColor()
+        {
+            Console.BackgroundColor = ConsoleColor.DarYellow;
+        }
+        public void SetFontColor()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+        }
+        public string GetHeader(int width)
+        {
+            return new string('~', width);
+        }
+        public string GetFooter(int width)
+        {
+            return new string('~', width);
+        }
+    }
+}
