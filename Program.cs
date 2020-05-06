fc01034f367883adc2b80b51d9f74da9662ee448
Amela Vorgić <Korisnik@DESKTOP-CON3K37>
2020-05-06 13:57:41 +0200

    zadatak 5

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV5RPPOON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LightTheme lightTheme = new LightTheme();
+            MyTheme yellowTheme = new MyTheme();
+            ReminderNote firstnote = new ReminderNote("Is this working?", lightTheme);
+            firstnote.Show();
+            ReminderNote secondnote = new ReminderNote("This one is pretty!", yellowTheme);
+            secondnote.Show();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReminderNote.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV5RPPOON
+{
+    class ReminderNote : Note
+    {
+        public ReminderNote(string message, ITheme theme) : base(message, theme) { }
+        public override void Show()
+        {
+            this.ChangeColor();
+            Console.WriteLine("REMINDER: ");
+            string framedMessage = this.GetFramedMessage();
+            Console.WriteLine(framedMessage);
+            Console.ResetColor();
+        }
+    }
+}
+
+
